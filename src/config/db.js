0068b20cec80fc15cce9c2cb48d0bd69f85698dc
manuelvg-og <manuelvg696@gmail.com>
2026-06-26 23:37:35 -0400

    cambios correctos

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: '://supabase.com',
-  port: 6543,
-  user: 'postgres.yqufltzfrgchkzikbbmp',
-  password: '5422000mv54mv',
-  database: 'postgres',
-  ssl: { rejectUnauthorized: false }
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.query('SELECT NOW()')
-  .then(() => console.log('✅ Conexión exitosa a Supabase'))
-  .catch(err => console.error('❌ Conexión fallida:', err.message));
+    .then(() => console.log('✅ Conexión exitosa a Supabase'))
+    .catch(err => console.error('❌ Conexión fallida:', err.message));
 
 module.exports = pool;
